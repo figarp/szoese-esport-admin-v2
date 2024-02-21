00c6c714e2714e93635fc0c6ae28bd7af2e7e983
figarp <figuraarpad03@gmail.com>
2024-02-21 17:36:58 +0100

    post image fix

--- resources/views/dashboard/posts/index.blade.php
+++ resources/views/dashboard/posts/index.blade.php
@@ -46,7 +46,7 @@
                                     <p class="card-text">{!! nl2br($post->slug) !!}</p>
                                 </div>
                             </div>
-                            @if ($post->image->path)
+                            @if ($post->image)
                                 <img src="{{ Storage::url($post->image->path) }}" alt="" class="indexImg">
                             @endif
                         </div>
